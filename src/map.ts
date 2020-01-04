ac6b5e6d257a7043dc8548e890a4867a9fa03c79
Joshua Barnes <joshuabarnes@Joshuas-MacBook-Pro.local>
2020-01-03 22:05:51 -0800

    Added map

---
+++ src/map.ts
@@ -0,0 +1,25 @@
+import { Tile } from "./tile";
+import tileset from './tileset';
+
+export class Map {
+    private tiles: number[][];
+
+    constructor(tiles: number[][]) {
+        this.tiles = tiles;
+    }
+
+    public getWidth(): number {
+        return this.tiles.length || 0;
+    }
+
+    public getHeight(): number {
+        return this.tiles.length > 0 ?
+            this.tiles[0].length
+            :
+            0;
+    }
+
+    public getTile(x: number, y: number): Tile {
+        return tileset[this.tiles[x][y]];
+    }
+}
---
+++ src/tileset.ts
@@ -0,0 +1,14 @@
+import { Tile } from "./tile";
+
+const tileset: { [key: number]: Tile } = {
+    0: {
+        description: "water",
+        color: "blue"
+    },
+    1: {
+        description: "water",
+        color: "green"
+    }
+};
+
+export default tileset;
